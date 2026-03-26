7baf35515b03e4cc96f5ca54fcf6ea2a0e85e950
Kaloyan Donev (aka MineManiack) <68351222+Kaloyan501@users.noreply.github.com>
2026-03-27 00:12:39 +0200

    Implement random enemy creation method
    Added a method to randomly create enemies based on a specified count.

--- Ultrakill_RPG/EnemyProperties.cs
+++ Ultrakill_RPG/EnemyProperties.cs
@@ -119,7 +119,31 @@ namespace Ultrakill_RPG
                 Texterer.InputEnemyNaming();
             }
         }
-        
+
+        public class voic EnemyCreatorRandom(int enemies){
+        for (int i = 0; i < enemies; i ++){
+            Random rnd = new Random();
+            
+            int enemyInt = rnd.Next(1, 4);
+            
+            if (enemyInt == 1)
+            {
+                EnemyList.AddEnemy(new Filth(),timesRepeated);
+            }
+            else if (enemyInt == 2)
+            {
+                EnemyList.AddEnemy(new Stray(),timesRepeated);
+            }
+            else if (enemyInt == 3)
+            {
+                EnemyList.AddEnemy(new Schism(),timesRepeated);
+            }
+            else if (enemyInt == 4)
+            {
+                EnemyList.AddEnemy(new Cerberus(),timesRepeated);
+            }
+        }
+    }
 
         /*public void Creator(string nameInput, int enemyCount)
         {
