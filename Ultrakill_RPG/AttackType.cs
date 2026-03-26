b6cc45abbcff1a62dadb2922e6a5d535220ed57f
Kaloyan Donev (aka MineManiack) <68351222+Kaloyan501@users.noreply.github.com>
2026-03-27 00:18:08 +0200

    Implement AttackType.cs

--- Ultrakill_RPG/AttackType.cs
+++ Ultrakill_RPG/AttackType.cs
@@ -1,1 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace Ultrakill_RPG
+{
+    public class AttackType
+    {
+      protected string name;
+      protected int damageDealth;
+
+      public Attack(string name, int damageDealth) {
+        this.name = name;
+        this.damageDealth = damageDealth;
+      }
+
+      public string GetAttackName(){
+        return this.name;
+      }
+
+      public int GetAttackDamage(){
+        return this.damageDealth;
+      }
+    }
+}
