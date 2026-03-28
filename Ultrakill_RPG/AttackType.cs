31126e6f8e6de17ac75b2a727950a1156f45fd42
bu11et-hell <plamenborislav@gmail.com>
2026-03-28 17:29:56 +0200

    fixed the spacing

--- Ultrakill_RPG/AttackType.cs
+++ Ultrakill_RPG/AttackType.cs
@@ -6,19 +6,19 @@ namespace Ultrakill_RPG
 {
     public class AttackType
     {
-      protected string name;
-      protected double damageDealth;
+        protected string name;
+        protected double damageDealth;
 
-      public AttackType(string name, double damageDealth) {
-        this.name = name;
-        this.damageDealth = damageDealth;
-      }
-      public string GetAttackName(){
-        return this.name;
-      }
+        public AttackType(string name, double damageDealth) {
+            this.name = name;
+            this.damageDealth = damageDealth;
+        }
+        public string GetAttackName(){
+            return this.name;
+        }
 
-      public double GetAttackDamage(){
-        return this.damageDealth;
-      }
+        public double GetAttackDamage(){
+            return this.damageDealth;
+        }
     }
 }
