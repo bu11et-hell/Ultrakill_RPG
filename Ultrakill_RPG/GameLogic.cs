a6ab700cbacea2195a4987f4448306a6765a0fbb
bu11et-hell <plamenborislav@gmail.com>
2026-03-29 19:17:53 +0300

    Completed GameLogic

--- Ultrakill_RPG/GameLogic.cs
+++ Ultrakill_RPG/GameLogic.cs
@@ -6,6 +6,8 @@ namespace Ultrakill_RPG
 {
     public class GameLogic
     {
+        private static GameObject enemyPlayerSelection;
+        private static AttackType enemyAttackSelection;
         public static string AttackMassage(GameObject attackerObject, GameObject selectedObject, AttackType selectedAttack)
         {
             return $"{attackerObject.GetName()} used {selectedAttack.GetAttackName()} and dealt {selectedObject.damageTaken} dmg to {selectedObject.GetName()}";
@@ -19,14 +21,27 @@ namespace Ultrakill_RPG
             AttackMassage(selectedPlayer, selectedEnemy, selectedAttack);
             Console.WriteLine($"The enemy is down to {selectedEnemy.GetHealth()} health");
             EnemyDeadChecker(selectedEnemy);
-            EnemyRandomTargetAndAttack();
+            EnemyAttackLogic();
         }
-        public static void EnemyRandomTargetAndAttack()
+        public static void EnemyAttackLogic(GameObject selectedEnemy, GameObject selectedPlayer)
+        {
+            EnemyRandomTargetAndAttack(selectedEnemy);
+            selectedEnemy.DealDamage(selectedEnemy, selectedPlayer, enemyAttackSelection);
+            AttackMassage(selectedEnemy, selectedPlayer, enemyAttackSelection);
+            Console.WriteLine($"The Player is down to {selectedEnemy.GetHealth()} health");
+            PlayerDeadChecker(selectedPlayer);
+            GameEndedCheck();
+        }
+        public static void EnemyRandomTargetAndAttack(GameObject selectedEnemy)
         {
             //attack choise rng
             Random rng = new Random();
 
+            int EnemyPlayerSelectionInt = rng.Next(0, PlayerList.players.Count());
+            enemyPlayerSelection = PlayerList.players[EnemyPlayerSelectionInt];
             //player choise rng
+            int EnemyAttackSelectionInt = rng.Next(0, selectedEnemy.attackList.Count());
+            enemyAttackSelection = selectedEnemy.attackList[EnemyPlayerSelectionInt];
         }
         public static void GameEndedCheck()
         {
@@ -37,6 +52,13 @@ namespace Ultrakill_RPG
             else if (EnemyList.enemies.Count() == 0)
             {
                 Console.WriteLine("You have won");
+            }
+        }
+        public static void PlayerDeadChecker(GameObject player)
+        {
+            if (player.GetHealth() <= 0)
+            {
+                PlayerList.RemovePlayer(player);
             }
         }
         public static void EnemyDeadChecker(GameObject enemy)
