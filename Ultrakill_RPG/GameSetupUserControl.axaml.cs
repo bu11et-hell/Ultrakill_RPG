27376fd3b6b70f0977285d7f019ebaf5472a5df7
Kaloyan Donev <donevkaloyan@gmail.com>
2026-03-29 19:08:56 +0300

    Add setup window prototye and background music processor prototype.

---
+++ Ultrakill_RPG/GameSetupUserControl.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Ultrakill_RPG;
+
+public partial class GameSetupUserControl : UserControl
+{
+    public GameSetupUserControl() => InitializeComponent();
+}
--- Ultrakill_RPG/MainWindow.axaml.cs
+++ Ultrakill_RPG/MainWindow.axaml.cs
@@ -1,13 +1,44 @@
+using System.ComponentModel;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using LibVLCSharp.Shared;
 
 namespace Ultrakill_RPG;
 
 public partial class MainWindow : Window
 {
+    private LibVLC? _libVlC;
+    private MediaPlayer? _mediaPlayer;
+    private Media? _media;
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    protected override void OnOpened(EventArgs e)
+    {
+        base.OnOpened(e);
+        
+        if (Design.IsDesignMode) return;
+        
+        Core.Initialize();
+        _libVlC = new LibVLC();
+        _mediaPlayer = new MediaPlayer(_libVlC);
+        _media = new Media(_libVlC, "/home/kaloyan/koko_programing/Ultrakill_RPG/Ultrakill_RPG/Ultrakill_RPG/Assets/Audio/test.mp3", FromType.FromPath);
+        _media.AddOption(":input-repeat=65535");
+        //_mediaPlayer.Play(_media);
+    }
+
+    protected override void OnClosing(WindowClosingEventArgs e)
+    {   
+        base.OnClosing(e);
+        
+        if (Design.IsDesignMode) return;
+        
+        _mediaPlayer?.Stop();
+        _media?.Dispose();
+        _mediaPlayer?.Dispose();
+        _libVlC?.Dispose();
     }
 }
